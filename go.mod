4271ff2c72d85e3f38e6ea74ce170fe030f4abe8
poramest.vic <poramest.vic@ascendcorp.com>
2023-02-20 15:23:49 +0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module tagDefaultStruct
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+)
+
+type Test struct {
+	Text   string `default:"default text"`
+	Number int32  `default:"123"`
+}
+
+func main() {
+	test := &Test{}
+	TagDefault(test)
+	fmt.Print(test)
+}
+
+func TagDefault(in interface{}) {
+	valueType := reflect.TypeOf(in).Elem()
+	value := reflect.ValueOf(in).Elem()
+	parserData := mapperParserData()
+	for i := 0; i < valueType.NumField(); i++ {
+		field := valueType.Field(i)
+		defaultTag := field.Tag.Get("default")
+		if defaultTag != "" {
+			fieldValue := value.FieldByName(field.Name)
+			if fieldValue.CanSet() {
+				parserData[fieldValue.Kind()](fieldValue, defaultTag)
+			}
+		}
+	}
+}
+
+func mapperParserData() map[reflect.Kind]func(reflect.Value, string) {
+	return map[reflect.Kind]func(reflect.Value, string){
+		reflect.String: func(fieldValue reflect.Value, defaultTag string) {
+			fieldValue.SetString(defaultTag)
+		},
+		reflect.Int: func(fieldValue reflect.Value, defaultTag string) {
+			defaultInt, err := strconv.ParseInt(defaultTag, 10, 64)
+			if err == nil {
+				fieldValue.SetInt(defaultInt)
+			}
+		},
+		reflect.Int8: func(fieldValue reflect.Value, defaultTag string) {
+			defaultInt, err := strconv.ParseInt(defaultTag, 10, 8)
+			if err == nil {
+				fieldValue.SetInt(defaultInt)
+			}
+		},
+		reflect.Int16: func(fieldValue reflect.Value, defaultTag string) {
+			defaultInt, err := strconv.ParseInt(defaultTag, 10, 16)
+			if err == nil {
+				fieldValue.SetInt(defaultInt)
+			}
+		},
+		reflect.Int32: func(fieldValue reflect.Value, defaultTag string) {
+			defaultInt, err := strconv.ParseInt(defaultTag, 10, 32)
+			if err == nil {
+				fieldValue.SetInt(defaultInt)
+			}
+		},
+		reflect.Int64: func(fieldValue reflect.Value, defaultTag string) {
+			defaultInt, err := strconv.ParseInt(defaultTag, 10, 64)
+			if err == nil {
+				fieldValue.SetInt(defaultInt)
+			}
+		},
+		reflect.Float32: func(fieldValue reflect.Value, defaultTag string) {
+			defaultFloat, err := strconv.ParseFloat(defaultTag, 32)
+			if err == nil {
+				fieldValue.SetFloat(defaultFloat)
+			}
+		},
+		reflect.Float64: func(fieldValue reflect.Value, defaultTag string) {
+			defaultFloat, err := strconv.ParseFloat(defaultTag, 64)
+			if err == nil {
+				fieldValue.SetFloat(defaultFloat)
+			}
+		},
+		reflect.Bool: func(fieldValue reflect.Value, defaultTag string) {
+			defaultBool, err := strconv.ParseBool(defaultTag)
+			if err == nil {
+				fieldValue.SetBool(defaultBool)
+			}
+		},
+	}
+}
